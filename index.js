2cb236f21658e0d0e0829b84751839bfdd1eef1b
Kipleting <kipletingedwin4@gmail.com>
2022-06-13 22:11:19 +0300

    commit tree 9617a14dd3875e5034725321477d6df425945470

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('The DOM has loaded');
+    const para=document.getElementById('text')
+    para.innerHTML='This is really cool!';
+
+})
